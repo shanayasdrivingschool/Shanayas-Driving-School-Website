0a3f9de4f2a334468d6bc07ccdf6174ecda52fd9
Bakkr123 <sheikhabubakr174@gmail.com>
2026-07-22 21:50:31 +0500

    Deploy: Promotions & Discounts policy, minimal policy pages, 400/401/403/500 pages

---
+++ assets/policyIcons-BoiDgH9f.js
@@ -0,0 +1,11 @@
+import{e}from"./index-CpkZhVtH.js";import{U as o}from"./users-FVjwEZWa.js";import{C as r}from"./credit-card-BfB8Vvf6.js";import{S as t}from"./shield-check-ChoNUW9B.js";import{F as i}from"./file-text-D-MRtnc7.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Gavel",[["path",{d:"m14.5 12.5-8 8a2.119 2.119 0 1 1-3-3l8-8",key:"15492f"}],["path",{d:"m16 16 6-6",key:"vzrcl6"}],["path",{d:"m8 8 6-6",key:"18bi4p"}],["path",{d:"m9 7 8 8",key:"5jnvq1"}],["path",{d:"m21 11-8-8",key:"z4y7zo"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=e("Percent",[["line",{x1:"19",x2:"5",y1:"5",y2:"19",key:"1x9vlm"}],["circle",{cx:"6.5",cy:"6.5",r:"2.5",key:"4mh3h7"}],["circle",{cx:"17.5",cy:"17.5",r:"2.5",key:"1mdrzq"}]]),n={"privacy-policy":t,"installment-policy":r,"in-vehicle-passenger-policy":o,"promotions-and-discounts":m,"terms-and-conditions":a},h=c=>n[c]??i;export{h as g};
