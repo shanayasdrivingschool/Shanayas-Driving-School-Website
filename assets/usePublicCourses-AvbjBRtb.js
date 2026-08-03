f5c33b54fa602ce8e5213745352111940670adbe
Bakkr123 <sheikhabubakr174@gmail.com>
2026-08-04 02:30:53 +0500

    Publish the beginner lessons landing page

--- assets/usePublicCourses-AvbjBRtb.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./useQuery-_cRQNPKv.js";import{g as s,a as u}from"./courseService-C3Ia5MQ8.js";const i=()=>e({queryKey:["public-courses"],queryFn:s,initialData:u(),staleTime:6e4});export{i as u};
