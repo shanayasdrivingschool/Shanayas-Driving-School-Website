e01fd513e191afbd8509db7de03bbaaa9e4d54da
Bakkr123 <sheikhabubakr174@gmail.com>
2026-08-04 03:46:44 +0500

    Publish dialog shift fix

---
+++ assets/usePublicCourses-CB5WQZKY.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useQuery-Bbk-a9_-.js";import{g as s,a as u}from"./courseService-BU4qT3Zu.js";const i=()=>e({queryKey:["public-courses"],queryFn:s,initialData:u(),staleTime:6e4});export{i as u};
