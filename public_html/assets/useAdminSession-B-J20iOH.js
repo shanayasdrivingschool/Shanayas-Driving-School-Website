76063e7e57559c6f07492deba4859e43c4e543cc
Bakkr123 <sheikhabubakr174@gmail.com>
2026-08-24 22:15:37 +0500

    Delete the unused template contact and footer components

---
+++ public_html/assets/useAdminSession-B-J20iOH.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useQuery-D_65KTm_.js";import{a as o}from"./index-DOWjeugj.js";import{getAdminSession as s}from"./affiliateApi-CKSKJ7tV.js";const m=()=>{const{user:e}=o();return n({queryKey:["admin-session",e==null?void 0:e.id],queryFn:s,enabled:!!e,staleTime:5*6e4,refetchInterval:5*6e4,refetchOnWindowFocus:!0})};export{m as u};
