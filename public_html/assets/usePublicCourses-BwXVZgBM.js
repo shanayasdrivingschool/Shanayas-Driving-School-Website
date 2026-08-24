d88b1caa6428d801313d608dce20f4ffd15886e5
Bakkr123 <sheikhabubakr174@gmail.com>
2026-08-24 21:23:37 +0500

    Publish Let a dry run report large deletions instead of stopping (2916810)

---
+++ public_html/assets/usePublicCourses-BwXVZgBM.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useQuery-CVwRp6fu.js";import{g as s,a as u}from"./courseService-BJXfsomo.js";const i=()=>e({queryKey:["public-courses"],queryFn:s,initialData:u(),staleTime:6e4});export{i as u};
