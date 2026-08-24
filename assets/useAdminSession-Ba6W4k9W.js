38bbc51fb88fe3421a9b8582432b75ef2360218d
Bakkr123 <sheikhabubakr174@gmail.com>
2026-08-24 22:06:26 +0500

    Publish Add school discretion, simplify school cancellations, drop the address (1bfd762)

---
+++ assets/useAdminSession-Ba6W4k9W.js
@@ -0,0 +1,1 @@
+import{u as n}from"./useQuery-DUKwVFpf.js";import{a as o}from"./index-S_zIIuuX.js";import{getAdminSession as s}from"./affiliateApi-BFHXStSD.js";const m=()=>{const{user:e}=o();return n({queryKey:["admin-session",e==null?void 0:e.id],queryFn:s,enabled:!!e,staleTime:5*6e4,refetchInterval:5*6e4,refetchOnWindowFocus:!0})};export{m as u};
