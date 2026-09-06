4c6555f3d937bd49215bf03bfc883faea5ee0e72
Muhammad Abu Bakar <muhammadabubakar@Muhammads-MacBook-Air.local>
2026-09-06 16:18:05 +0500

    Publish Update website and SEO content (f060e72a)

---
+++ assets/usePublicCourses-ClYM6yoB.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useQuery-Q67vC3px.js";import{g as s,a as u}from"./courseService-D8udW5xd.js";const i=()=>e({queryKey:["public-courses"],queryFn:s,initialData:u(),staleTime:6e4});export{i as u};
